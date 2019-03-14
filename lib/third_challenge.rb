43458f01dd206edd57f69bb86d09ec56c447a888
Adnan Azim <saazim1123@gmail.com>
2019-03-14 01:10:48 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,6 +22,7 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        name: "Lord Capulet"
         },
       :matriarch => {
         #your key/value pairs here
