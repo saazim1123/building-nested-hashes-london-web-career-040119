c43477b2a0f893a43b767482eeb33d02772dc6a0
Adnan Azim <saazim1123@gmail.com>
2019-03-14 01:11:25 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,6 +27,8 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        name: "Lady Capulet"
+        age: "15"
         },
       :heroine => {
         #your key/value pairs here
